725c11e7fb11e2c9aac6a06208f0da483aeaf5f0
Roman Berezkin <Glitchy-Sheep@users.noreply.github.com>
2025-01-31 00:36:40 +0300

    feat(better-output): use float/datetime data in parser because it's much more flexible than just get a direct result

--- src/handbrake_cli_output_capturer.py
+++ src/handbrake_cli_output_capturer.py
@@ -6,31 +6,31 @@ from typing import Optional
 
 
 class HandbrakeProgressInfo(BaseModel):
-    progress: str
-    fps_current: Optional[str]
-    fps_average: Optional[str]
-    eta: Optional[str]
+    progress: Optional[float]
+    fps_current: Optional[float]
+    fps_average: Optional[float]
+    eta: Optional[datetime.timedelta]
 
 
 def parse_handbrake_cli_output(line: str) -> HandbrakeProgressInfo:
     # Extract percentage
     progress_match = re.search(r"(\d+\.\d+) %", line)
-    progress = f"{progress_match.group(1)}%" if progress_match else None
+    progress = float(progress_match.group(1)) if progress_match else None
 
     # Get current FPS
     fps_current_match = re.search(r"([\d.]+) fps", line)
-    fps_current = f"{fps_current_match.group(1)} fps" if fps_current_match else None
+    fps_current = float(fps_current_match.group(1)) if fps_current_match else None
 
     # Get Average FPS
     fps_avg_match = re.search(r"avg ([\d.]+) fps", line)
-    fps_avg = f"{fps_avg_match.group(1)} fps" if fps_avg_match else None
+    fps_avg = float(fps_avg_match.group(1)) if fps_avg_match else None
 
     # Extract ETA
     eta_match = re.search(r"ETA (\d+h\d+m\d+s)", line)
     eta = None
     if eta_match:
         h, m, s = map(int, re.findall(r"\d+", eta_match.group(1)))
-        eta = str(datetime.timedelta(hours=h, minutes=m, seconds=s))
+        eta = datetime.timedelta(hours=h, minutes=m, seconds=s)
 
     return HandbrakeProgressInfo(
         progress=progress,
--- test/handbrake_cli_output_capturer_test.py
+++ test/handbrake_cli_output_capturer_test.py
@@ -1,3 +1,5 @@
+import datetime
+
 from src.handbrake_cli_output_capturer import (
     parse_handbrake_cli_output,
 )
@@ -12,10 +14,10 @@ class TestHandbrakeCliOutputCapturer:
 
         progress_info = parse_handbrake_cli_output(handbrake_cli_output)
 
-        assert progress_info.progress == "4.00%"
-        assert progress_info.fps_current == "937.13 fps"
-        assert progress_info.fps_average == "955.64 fps"
-        assert progress_info.eta == "0:03:22"
+        assert progress_info.progress == 4.00
+        assert progress_info.fps_current == 937.13
+        assert progress_info.fps_average == 955.64
+        assert progress_info.eta == datetime.timedelta(hours=0, minutes=3, seconds=22)
 
     def test_completed(self):
         handbrake_cli_output = (
@@ -24,17 +26,17 @@ class TestHandbrakeCliOutputCapturer:
 
         progress_info = parse_handbrake_cli_output(handbrake_cli_output)
 
-        assert progress_info.progress == "100.00%"
-        assert progress_info.fps_current == "0 fps"
-        assert progress_info.fps_average == "0 fps"
-        assert progress_info.eta == "0:00:00"
+        assert progress_info.progress == 100.00
+        assert progress_info.fps_current == 0
+        assert progress_info.fps_average == 0
+        assert progress_info.eta == datetime.timedelta(hours=0, minutes=0, seconds=0)
 
     def test_without_statistics(self):
         handbrake_cli_output = "Encoding: task 1 of 1, 12.00 % "
 
         progress_info = parse_handbrake_cli_output(handbrake_cli_output)
 
-        assert progress_info.progress == "12.00%"
+        assert progress_info.progress == 12.00
         assert progress_info.fps_current == None
         assert progress_info.fps_average == None
         assert progress_info.eta == None
