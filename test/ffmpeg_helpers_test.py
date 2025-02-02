9dc439e3897aac49022ca1ad4d58039ce83ec10f
Roman Berezkin <Glitchy-Sheep@users.noreply.github.com>
2025-02-02 21:10:19 +0300

    test: :white_check_mark: Add tests for new functions from ffmpeg_helpers module

--- test/ffmpeg_helpers_test.py
+++ test/ffmpeg_helpers_test.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 
-from src.ffmpeg_helpers import get_video_resolution
+from src.ffmpeg_helpers import (
+    get_video_bitrate,
+    get_video_frame_rate,
+    get_video_resolution,
+)
 
 
 def test_get_video_resolution(video_720p_2mb_mp4: Path):
@@ -9,3 +13,15 @@ def test_get_video_resolution(video_720p_2mb_mp4: Path):
     assert resolution.width == 1280
     assert resolution.height == 720
     assert resolution.area == 921600
+
+
+def test_get_video_bitrate(video_720p_2mb_mp4: Path):
+    bitrate = get_video_bitrate(video_720p_2mb_mp4)
+
+    assert bitrate == 842
+
+
+def test_get_video_frame_rate(video_720p_2mb_mp4: Path):
+    frame_rate = get_video_frame_rate(video_720p_2mb_mp4)
+
+    assert frame_rate == 25.0
