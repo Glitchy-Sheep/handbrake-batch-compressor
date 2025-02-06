ae97e29bb0be36a99e138b9cb311bfdf25c2cef2
Roman Berezkin <Glitchy-Sheep@users.noreply.github.com>
2025-02-06 07:35:23 +0300

    docs: :memo: Enhance help messages a little, remove redundant text, add more examples

--- main.py
+++ main.py
@@ -121,6 +121,7 @@ def main(  # noqa: PLR0913: too many arguments because of typer
             '-r',
             help='The minimum resolution. Videos below this threshold will be skipped.',
             parser=VideoResolution.parse_resolution,
+            metavar='<WIDTH>x<HEIGHT>',
         ),
     ] = None,
     keep_only_smaller: Annotated[
@@ -128,29 +129,24 @@ def main(  # noqa: PLR0913: too many arguments because of typer
         typer.Option(
             '--keep-only-smaller',
             '-k',
-            help='Should only videos smaller than the original be kept.',
+            help='Should only videos smaller than the original be kept. If used with -d and files are larger than the original, they will [bold]not be deleted.[/bold]',
         ),
     ] = False,
 ) -> None:
     """
     Compress your video files in batch with HandbrakeCLI.
 
-    1. Set `--target-path` to the directory containing your videos.
-    2. The utility will compress all video files in it recursively.
-
-    Use `--delete-original-files` to replace the original files with compressed ones.
-    ⚠ [bold red]Be cautious![/bold red] This deletes the original videos right after compression.
-
-    The utility uses file extensions to skip already compressed videos.
-    Customize with `--progress-extension` and `--complete-extension` (ensure they are unique and do not contain dots).
-
     [bold green]Examples:[/bold green]
     1. Compress all videos in `./videos` and delete originals:
     - [bold] ./main.py -t ./videos -d [/bold]
     2. Compress files with custom encoder and quality:
-    - [bold] ./main.py -t ./videos --handbrakecli-options "--encoder qsv_h264 --quality 20" [/bold]
+    - [bold] ./main.py -t ./videos -o "--encoder qsv_h264 --quality 20" [/bold]
     3. Compress using a preset:
-    - [bold] ./main.py -t ./videos --handbrakecli-options "--preset 'Fast 720p30'" [/bold]
+    - [bold] ./main.py -t ./videos -o "--preset 'Fast 720p30'" [/bold]
+    4. Compress files and leave only those smaller than the original and show stats:
+    - [bold] ./main.py -t ./videos -k -s [/bold]
+    5. Compress files excluding files with resolution and bitrate lower than the specified ones:
+    - [bold] ./main.py -t ./videos --filter-min-resolution 720x480 --filter-min-bitrate 100 [/bold]
     """
     check_target_path(target_path)
     check_extensions_arguments(progress_ext, complete_ext)
